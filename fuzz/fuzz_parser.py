9e14e4091977ce750b9fc76e75101c5ad2545054
Mat <mail@mathias.is>
2026-09-04 20:00:44 +0300

    fuzz

---
+++ fuzz/fuzz_parser.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2026 tinytag Contributors
+# SPDX-License-Identifier: MIT
+
+# pylint: disable=import-error,missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pyright: reportAttributeAccessIssue=false,reportMissingModuleSource=false
+# pyright: reportUnknownMemberType=false
+
+import io
+import sys
+
+import atheris
+
+from tinytag import TinyTag, TinyTagException
+
+
+def test_one_input(data: bytes) -> None:
+    try:
+        tag = TinyTag.get(file_obj=io.BytesIO(data))
+    except TinyTagException:
+        return
+
+    tag.as_dict()
+
+
+atheris.instrument_all()
+atheris.Setup(sys.argv, test_one_input)
+atheris.Fuzz()
